5f8639a7577440b4bda69f928ce326551709e463
Christian <cristhianxd.no.95@gmail.com>
2026-04-09 14:47:07 -0300

    agregue otro for

--- 06-Bucles/Ejercicio-for01.py
+++ 06-Bucles/Ejercicio-for01.py
@@ -2,3 +2,8 @@ articulos = ["guantes", "gorras", "bufandas"]
 
 for mercaderia in articulos:
     print ( "lo que hay en stock  " +  mercaderia)
+
+contador = 0
+
+for contador in range (100, 200):
+    print(f"Voy por el :  + str {contador}")
