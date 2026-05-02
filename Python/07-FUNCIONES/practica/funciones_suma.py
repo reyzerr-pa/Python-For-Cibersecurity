06bfce5623cf0c97678fd3a179c29e7c4bd2f5a7
Christian <cristhianxd.no.95@gmail.com>
2026-05-02 14:14:56 -0300

    Funciones con parametros y listas

---
+++ Python/07-FUNCIONES/practica/funciones_suma.py
@@ -0,0 +1,34 @@
+def suma(x, y, z):
+    resultado = x + y + z
+    print(resultado)
+
+suma(1, 2, 3)
+suma(4, 5, 6)
+suma(7, 8, 9)
+#LLamo 3 veces a la función suma, cada vez con diferentes números,
+# y me imprime el resultado de la suma de esos números.
+
+
+valores = input("Ingresa tres números separados por comas: ").split(",")
+valores = [int(valor) for valor in valores]
+
+def resta():
+    resultado = valores[0] - valores[1] - valores[2]
+    print(resultado)    
+
+resta()
+#Defino una lista de valores y 
+# luego creo una función que resta esos valores.   
+
+
+#Return es una palabra reservada que se utiliza para devolver 
+# un valor desde una función.
+#Cuando se ejecuta una función, el código dentro de la función 
+# se ejecuta y luego
+
+def multiplicacion(a, b):
+    resultado = a * b
+    return resultado
+
+# Ejemplo de uso
+print(multiplicacion(5, 3))  # Imprime 15
